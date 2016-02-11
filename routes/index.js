b33cb99b397986ba92aff6d6b1f4e2813497eb6b
RobReynolds <rob_reynolds_10@hotmail.com>
2016-02-10 23:46:51 -0500

    added pages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET home page */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Robert Reynolds' });
+});
+
+/* GET about page */
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About Me' });
+});
+
+/* Get contact page */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact Me' });
 });
 
 module.exports = router;
